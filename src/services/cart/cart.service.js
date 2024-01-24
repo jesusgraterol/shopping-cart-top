225e040de7530ff6b0307e911e73d084bbc9c745
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-01-24 15:15:20 -0400

    Implement the CartService as well as its unit tests
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/services/cart/cart.service.js
+++ src/services/cart/cart.service.js
@@ -1,32 +1,159 @@
+import NumberUtilities from '../shared/number/number.utilities.js';
 import Database from '../shared/database/database.service.js';
 
 /**
  * Cart Service
- * ...
+ * Service in charge of handling all the operations that can be performed in the shopping cart.
  */
 class CartService {
+  // the key used to store the cart record in the db
+  static #DB_KEY = 'cartRecord';
+
   // the main cart record
-  static #record = CartService.#buildDefaultRecord();
+  static #record;
 
-  static getRecord() {
 
+
+
+
+  /* *********
+   * GETTERS *
+   ********* */
+
+  static get totalQuantity() {
+    if (!CartService.#record) CartService.#initializeRecord();
+    return CartService.#record.totalQuantity;
   }
 
+  static get totalAmount() {
+    if (!CartService.#record) CartService.#initializeRecord();
+    return CartService.#record.totalAmount;
+  }
+
+  static get items() {
+    if (!CartService.#record) CartService.#initializeRecord();
+    return Object.values(CartService.#record.items);
+  }
+
+
+
+
+
+  /* **************
+   * CART ACTIONS *
+   ************** */
+
+  /**
+   * Adds a product to the cart.
+   * @param {*} product 
+   */
   static add(product) {
+    // validate the request
+    if (CartService.#record.items[product.id]) {
+      throw new Error(`The product with an ID of: ${product.id} cannot be added to the cart because it already exists.`);
+    }
 
+    // add product to the items obj
+    CartService.#record.items[product.id] = { quantity: 1, amount: product.price, product: product };
+
+    // trigger the on change event
+    CartService.#onCartChanges();
   }
 
+  /**
+   * Increases the quantity for a given product on the cart.
+   * @param {*} productID 
+   */
   static increaseQuantity(productID) {
+    // validate the request
+    if (!CartService.#record.items[productID]) {
+      throw new Error(`The quantity of the product ID ${productID} cannot be increased because it is not in the cart.`);
+    }
 
+    // increase the quantity and recalculate the amount
+    CartService.#record.items[productID].quantity += 1;
+    CartService.#record.items[productID].amount = CartService.#calculateItemAmount(
+      CartService.#record.items[productID].product.price,
+      CartService.#record.items[productID].quantity
+    );
+
+    // trigger the on change event
+    CartService.#onCartChanges();
   }
 
+  /**
+   * Decreases the quantity for a given product on the cart.
+   * @param {*} productID 
+   */
   static decreaseQuantity(productID) {
+    // validate the request
+    if (!CartService.#record.items[productID]) {
+      throw new Error(`The quantity of the product ID ${productID} cannot be decreased because it is not in the cart.`);
+    }
+    if (CartService.#record.items[productID].quantity === 1) {
+      throw new Error(`The quantity of the product ID ${productID} cannot be decreased because there is only one in the cart. Delete the item instead.`);
+    }
 
+    // increase the quantity and recalculate the amount
+    CartService.#record.items[productID].quantity -= 1;
+    CartService.#record.items[productID].amount = CartService.#calculateItemAmount(
+      CartService.#record.items[productID].product.price,
+      CartService.#record.items[productID].quantity
+    );
+
+    // trigger the on change event
+    CartService.#onCartChanges();
   }
 
+  /**
+   * Fully removes a product from the cart.
+   * @param {*} productID 
+   */
   static delete(productID) {
+    // validate the request
+    if (!CartService.#record.items[productID]) {
+      throw new Error(`The product ID ${productID} cannot be deleted because it is not in the cart.`);
+    }
 
+    // delete the item from the record
+    delete CartService.#record.items[productID];
+
+
+    // trigger the on change event
+    CartService.#onCartChanges();
   }
+
+
+
+
+
+  /* ***************
+   * EVENT HANDLER *
+   *************** */
+
+  /**
+   * Triggers whenever the cart experiences a change of any kind and recalculates the total items
+   * in the cart as well as the dollar amount.
+   */
+  static #onCartChanges() {
+    // calculate the new totals
+    const { totalQuantity, totalAmount } = Object.values(CartService.#record.items).reduce(
+      (accum, current) => {
+        accum.totalQuantity += current.quantity;
+        accum.totalAmount = accum.totalAmount.plus(current.amount);
+        return accum;
+      },
+      { totalQuantity: 0, totalAmount: NumberUtilities.getBigNumber(0) }
+    );
+
+    // set the new totals
+    CartService.#record.totalQuantity = totalQuantity;
+    CartService.#record.totalAmount = NumberUtilities.buildNumber(totalAmount);
+
+    // finally, update the record
+    Database.write(CartService.#DB_KEY, CartService.#record);
+  }
+
 
 
 
@@ -36,17 +163,41 @@ class CartService {
    ************** */
 
   /**
+   * Initializes the cart record in case it hasn't been. If no data is found in the db, it builds
+   * the default record object.
+   */
+  static #initializeRecord() {
+    if (!CartService.#record) {
+      CartService.#record = Database.read(CartService.#DB_KEY) || CartService.#buildDefaultRecord();
+    }
+  }
+
+  /**
    * Builds the default record object. This function is invoked when the app is initialized.
    * @returns ICartRecord
    */
   static #buildDefaultRecord() {
-    return {
-      totalQuantity: 0,
-      totalAmount: 0,
-      items: {}
-    }
+    return { totalQuantity: 0, totalAmount: 0, items: {} }
   }
 
+  /**
+   * Calculates the dollar amount for an item based on the product price and quantity.
+   * @param {*} price 
+   * @param {*} quantity 
+   * @returns number
+   */
+  static #calculateItemAmount(price, quantity) {
+    return NumberUtilities.buildNumber(NumberUtilities.getBigNumber(price).times(quantity));
+  }
+
+  /**
+   * Prettifies a given dollar amount so it can be displayed to users.
+   * @param {*} amount 
+   * @returns string
+   */
+  static prettifyAmount(amount) {
+    return NumberUtilities.prettifyNumber(amount, { prefix: '$' });
+  }
 }
 
 
---
+++ src/services/cart/cart.service.test-unit.js
@@ -0,0 +1,243 @@
+import NumberUtilities from '../shared/number/number.utilities.js';
+import CartService from './cart.service.js';
+import { MOCK_PRODUCTS } from './test-data';
+
+
+
+// Mock the entire DatabaseService
+jest.mock('../shared/database/database.service.js');
+
+
+
+/**
+ * Cart Initialization
+ * Suite in charge of testing that the cart initializes correctly.
+ */
+describe('Cart Initialization', () => {
+  beforeAll(() => { });
+
+  afterAll(() => { });
+
+  beforeEach(() => { });
+
+  afterEach(() => { });
+
+  test('can initialize the cart through the getter functions', () => {
+    expect(CartService.totalQuantity).toBe(0);
+    expect(CartService.totalAmount).toBe(0);
+    expect(CartService.items).toEqual([]);
+  });
+});
+
+
+
+
+/**
+ * Cart Actions
+ * Suite in charge of testing all the actions that can be performed in the cart.
+ */
+const clearCart = () => CartService.items.forEach((item) => CartService.delete(item.product.id) );
+describe('Cart Actions', () => {
+  beforeAll(() => { clearCart() });
+
+  afterAll(() => { });
+
+  beforeEach(() => { });
+
+  afterEach(() => { clearCart() });
+
+  test('can add several products', () => {
+    const items = [];
+    const products = MOCK_PRODUCTS.slice(0, 3);
+
+    CartService.add(products[0]);
+    expect(CartService.totalQuantity).toBe(1);
+    expect(CartService.totalAmount).toBe(products[0].price);
+    items.push({ quantity: 1, amount: products[0].price, product: products[0]});
+    expect(CartService.items).toEqual(items);
+
+    CartService.add(products[1]);
+    expect(CartService.totalQuantity).toBe(2);
+    expect(CartService.totalAmount).toBe(NumberUtilities.calculateSum(
+      [products[0].price, products[1].price]
+    ));
+    items.push({ quantity: 1, amount: products[1].price, product: products[1]});
+    expect(CartService.items).toEqual(items);
+
+
+    CartService.add(products[2]);
+    expect(CartService.totalQuantity).toBe(3);
+    expect(CartService.totalAmount).toBe(NumberUtilities.calculateSum(
+      [products[0].price, products[1].price, products[2].price]
+    ));
+    items.push({ quantity: 1, amount: products[2].price, product: products[2]});
+    expect(CartService.items).toEqual(items);
+  });
+
+  test('cannot add a product that has already been added', () => {
+    const products = MOCK_PRODUCTS.slice(0, 1);
+    CartService.add(products[0]);
+    expect(() => CartService.add(products[0])).toThrow();
+  });
+
+  test('can add several products and increase the quantity of one', () => {
+    const products = MOCK_PRODUCTS.slice(0, 3);
+
+    CartService.add(products[0]);
+    CartService.add(products[1]);
+    CartService.add(products[2]);
+
+    expect(CartService.totalQuantity).toBe(3);
+    expect(CartService.totalAmount).toBe(NumberUtilities.calculateSum(products.map((p) => p.price)));
+
+    CartService.increaseQuantity(products[1].id);
+    CartService.increaseQuantity(products[1].id);
+
+    const increasedItem = CartService.items.find((item) => item.product.id === products[1].id);
+    const increasedProductAmount = NumberUtilities.buildNumber(
+      NumberUtilities.getBigNumber(increasedItem.product.price).times(3)
+    );
+    expect(increasedItem.quantity).toBe(3);
+    expect(increasedItem.amount).toBe(increasedProductAmount);
+
+    expect(CartService.totalQuantity).toBe(5);
+    expect(CartService.totalAmount).toBe(NumberUtilities.calculateSum(
+      [
+        increasedProductAmount, 
+        ...products.map((p) => { 
+          if (p.id !== products[1].id) { return p.price } else { return 0 }  
+        })
+      ]
+    ));
+
+    expect(CartService.items).toEqual([
+      { quantity: 1, amount: products[0].price, product: products[0] },
+      { quantity: 3, amount: increasedProductAmount, product: products[1] },
+      { quantity: 1, amount: products[2].price, product: products[2] },
+    ]);
+  });
+
+  test('cannot increase a product that has not yet been added', () => {
+    expect(() => CartService.increaseQuantity(MOCK_PRODUCTS[0].id)).toThrow();
+  });
+
+  test('can add several products and decrease the quantity of one', () => {
+    const products = MOCK_PRODUCTS.slice(0, 3);
+
+    CartService.add(products[0]);
+    CartService.add(products[1]);
+    CartService.add(products[2]);
+
+    CartService.increaseQuantity(products[2].id);
+    CartService.increaseQuantity(products[2].id);
+    CartService.increaseQuantity(products[2].id);
+
+    const increasedItem = CartService.items.find((item) => item.product.id === products[2].id);
+    expect(increasedItem.quantity).toBe(4);
+    expect(increasedItem.amount).toBe(NumberUtilities.buildNumber(
+      NumberUtilities.getBigNumber(increasedItem.product.price).times(4)
+    ));
+
+    CartService.decreaseQuantity(products[2].id);
+    CartService.decreaseQuantity(products[2].id);
+
+    const decreasedItem = CartService.items.find((item) => item.product.id === products[2].id);
+    const decreasedItemAmount = NumberUtilities.buildNumber(
+      NumberUtilities.getBigNumber(decreasedItem.product.price).times(2)
+    );
+    expect(decreasedItem.quantity).toBe(2);
+    expect(decreasedItem.amount).toBe(decreasedItemAmount);
+
+    expect(CartService.totalQuantity).toBe(4);
+    expect(CartService.totalAmount).toBe(NumberUtilities.calculateSum(
+      [
+        decreasedItemAmount, 
+        ...products.map((p) => { 
+          if (p.id !== products[2].id) { return p.price } else { return 0 }  
+        })
+      ]
+    ));
+
+    expect(CartService.items).toEqual([
+      { quantity: 1, amount: products[0].price, product: products[0] },
+      { quantity: 1, amount: products[1].price, product: products[1] },
+      { quantity: 2, amount: decreasedItemAmount, product: products[2] },
+    ]);
+  });
+
+  test('cannot decrease a product that has not yet been added', () => {
+    expect(() => CartService.decreaseQuantity(MOCK_PRODUCTS[0].id)).toThrow();
+  });
+
+  test('cannot decrease a product if only 1 item has been added', () => {
+    const products = MOCK_PRODUCTS.slice(0, 1);
+    CartService.add(products[0]);
+    expect(() => CartService.decreaseQuantity(products[0].id)).toThrow();
+  });
+
+  test('can add and delete an item', () => {
+    const products = MOCK_PRODUCTS.slice(0, 1);
+
+    CartService.add(products[0]);
+    expect(CartService.totalQuantity).toBe(1);
+    expect(CartService.totalAmount).toBe(products[0].price);
+    expect(CartService.items).toEqual([
+      { quantity: 1, amount: products[0].price, product: products[0] },
+    ]);
+
+    CartService.delete(products[0].id);
+    expect(CartService.totalQuantity).toBe(0);
+    expect(CartService.totalAmount).toBe(0);
+    expect(CartService.items).toEqual([]);
+  });
+
+  test('can add several, increase and decrease the quantities and then delete them all', () => {
+    const products = MOCK_PRODUCTS.slice(0, 3);
+
+    CartService.add(products[0]);
+    CartService.add(products[1]);
+    CartService.add(products[2]);
+
+    CartService.increaseQuantity(products[0].id);
+    CartService.increaseQuantity(products[0].id);
+    CartService.increaseQuantity(products[1].id);
+    CartService.increaseQuantity(products[1].id);
+    CartService.increaseQuantity(products[1].id);
+    CartService.increaseQuantity(products[2].id);
+    CartService.increaseQuantity(products[2].id);
+
+    CartService.decreaseQuantity(products[0].id);
+    CartService.decreaseQuantity(products[1].id);
+
+    CartService.delete(products[0].id);
+    CartService.delete(products[1].id);
+    CartService.delete(products[2].id);
+
+    expect(CartService.totalQuantity).toBe(0);
+    expect(CartService.totalAmount).toBe(0);
+    expect(CartService.items).toEqual([]);
+  });
+
+  test('cannot delete an item that has not yet been added', () => {
+    expect(() => CartService.delete(MOCK_PRODUCTS[0].id)).toThrow();
+  });
+});
+
+
+/**
+ * Misc Helpers
+ * Suite in charge of testing all the misc helpers within the service.
+ */
+describe('Misc Helpers', () => {
+  beforeAll(() => { });
+
+  afterAll(() => { });
+
+  beforeEach(() => { });
+
+  afterEach(() => { });
+
+  test('can prettify a dollar amount', () => {
+    expect(CartService.prettifyAmount(7194556.3681)).toBe('$7,194,556.37');
+  });
+});
